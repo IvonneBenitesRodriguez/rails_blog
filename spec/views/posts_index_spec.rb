46aee0fe9389a82eee4ef82654c165ccd0f0d435
Ivonne <ivonnebenites2015@gmail.com>
2023-12-21 09:42:25 -0500

    Update files

--- spec/views/posts_index_spec.rb
+++ spec/views/posts_index_spec.rb
@@ -2,96 +2,79 @@ require 'rails_helper'
 
 RSpec.describe 'User Features', type: :feature do
   before do
-    @doctor = User.create!(id: 5, name: 'Doctor',
-                           photo:
-      'https://upload.wikimedia.org/wikipedia/commons/
-        4/41/Profile-720.png',
-                           bio: 'Mastology doctor')
-    @patient = User.create!(id: 6, name: 'Patient',
-                            photo:
-    'https://upload.wikimedia.org/wikipedia/commons/4/41/Profile-720.png',
-                            bio: 'Advent Hospital patient')
+    @officer = User.create!(id: 5, name: 'Officer',
+                            photo: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Profile-720.png',
+                            bio: 'Law enforcement officer')
+    @student = User.create!(id: 6, name: 'Student',
+                            photo: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Profile-720.png',
+                            bio: 'High school student')
 
-    Post.create!(title: 'Welcome', text: 'Welcome to my site.',
-                 comments_counter: 0, likes_counter: 2,
-                 author: @doctor)
+    Post.create!(title: 'Introduction', text: 'Hello, this is my introduction.', comments_counter: 0, likes_counter: 2,
+                 author: @officer)
+    Post.create!(title: 'Meeting new people', text: 'Hi, everyone! Nice to meet you.', comments_counter: 0,
+                 likes_counter: 2, author: @officer)
+    Post.create!(title: 'Police life', text: 'Sharing insights into my life as a police officer.', comments_counter: 0,
+                 likes_counter: 2, author: @officer)
+    Post.create!(title: 'Law enforcement challenges', text: 'Discussing challenges faced in law enforcement.',
+                 comments_counter: 0, likes_counter: 2, author: @officer)
+    Post.create!(title: 'Day in the life', text: 'Giving you a glimpse of my typical day.', comments_counter: 0,
+                 likes_counter: 2, author: @officer)
 
-    Post.create!(title: 'Meeting nice people!!', text: 'Hi, nice to meet you.',
-                 comments_counter: 0,
-                 likes_counter: 2, author: @doctor)
-
-    Post.create!(title: 'Doctor life', text: 'Sharing experiences as a doctor.',
-                 comments_counter: 0,
-                 likes_counter: 2, author: @doctor)
-
-    Post.create!(title: 'Tips to care your health',
-                 text: 'Discussing tips to care your health.',
-                 comments_counter: 0, likes_counter: 2,
-                 author: @doctor)
-
-    Post.create!(title: 'News for my patients', text: 'new treatments.',
-                 comments_counter: 0, likes_counter: 2, author: @doctor)
-
-    @patient_post = Post.create!(title: 'First day at the hospital!',
-                                 text: 'Exciting times on my first day!',
-                                 comments_counter: 0, likes_counter: 2, author: @patient)
-    @comment1 = Comment.create(post: @patient_post, user:
-    @patient, text: 'My comment on the student post')
-    @like1 = Like.create(post: @patient_post, user: @patient)
+    @student_post = Post.create!(title: 'First day at school', text: 'Exciting times on my first day!',
+                                 comments_counter: 0, likes_counter: 2, author: @student)
+    @comment1 = Comment.create(post: @student_post, user: @student, text: 'My comment on the student post')
+    @like1 = Like.create(post: @student_post, user: @student)
   end
 
   describe 'User Profile Page' do
     it 'displays user profile information' do
-      visit user_posts_path(@patient)
-      expect(page).to have_css("img[src*='#{@patient.photo}']")
-      expect(page).to have_content('Patient')
+      visit user_posts_path(@student)
+      expect(page).to have_css("img[src*='#{@student.photo}']")
+      expect(page).to have_content('Student')
     end
 
     it 'displays the number of posts' do
-      visit user_posts_path(@patient)
-      expect(@patient.posts.count).to have_content(1)
+      visit user_posts_path(@student)
+      expect(@student.posts.count).to have_content(1)
     end
 
     it 'displays a pagination button for posts' do
-      visit user_posts_path(@doctor)
+      visit user_posts_path(@officer)
       expect(page).to have_css('.pagination-button')
     end
   end
 
   describe 'User Post Page' do
     it 'displays post details' do
-      visit user_posts_path(@patient)
-      expect(@patient.posts[0].title).to have_content('First day at the hospital')
-      expect(@patient.posts[0].text).to have_content('Exciting times on my first day!')
+      visit user_posts_path(@student)
+      expect(@student.posts[0].title).to have_content('First day at school')
+      expect(@student.posts[0].text).to have_content('Exciting times on my first day!')
     end
 
     it 'displays the first comment and its count' do
-      visit user_post_path(@patient, @patient_post)
-      expect(page).to have_content('My comment on the patient post')
-      expect(@patient.posts[0].comments.count).to have_content(1)
+      visit user_post_path(@student, @student_post)
+      expect(page).to have_content('My comment on the student post')
+      expect(@student.posts[0].comments.count).to have_content(1)
     end
 
     it 'displays the number of likes' do
-      visit user_post_path(@patient, @patient_post)
-      expect(@patient.posts[0].likes.count).to have_content(1)
+      visit user_post_path(@student, @student_post)
+      expect(@student.posts[0].likes.count).to have_content(1)
     end
 
-    it 'redirects to the specific post when the user
-        clicks on it' do
-      visit user_path(@patient)
-      click_link @patient_post.title
-      expect(current_path).to eq(user_post_path(@patient, @patient_post))
+    it 'redirects to the specific post when the user clicks on it' do
+      visit user_path(@student)
+      click_link @student_post.title
+      expect(current_path).to eq(user_post_path(@student, @student_post))
     end
 
-    it 'displays a section for pagination if there are more posts than fit on the
-        view' do
+    it 'displays a section for pagination if there are more posts than fit on the view' do
       5.times do |n|
-        Post.create!(title: "Test Post #{n + 1}", text: "Test post text
-                #{n + 1}",
-                     comments_counter: 0, likes_counter: 2, author: @patient)
+        Post.create!(title: "Test Post #{n + 1}", text: "Test post text #{n + 1}",
+                     comments_counter: 0, likes_counter: 2, author: @student)
       end
 
-      visit user_posts_path(@patient)
+      visit user_posts_path(@student)
       expect(page).to have_css('.pagination-button')
     end
   end
--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -2,83 +2,79 @@ require 'rails_helper'
 
 RSpec.describe 'User Features', type: :feature do
   before do
-    @doctor = User.create!(id: 5, name: 'Mauricio',
-                           photo: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Profile-720.png',
-                           bio: 'Mastology Doctor')
-    @advent_hospital_patient = User.create!(id: 6, name: 'Lily',
-                                            photo:
-    'https://upload.wikimedia.org/wikipedia/commons/4/41/Profile-720.png',
-                                            bio: 'Advent Hospital patient')
+    @law_enforcement_officer = User.create!(id: 5, name: 'Tom',
+                                            photo: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Profile-720.png',
+                                            bio: 'Law enforcement officer')
+    @high_school_student = User.create!(id: 6, name: 'Lily',
+                                        photo: 'https://upload.wikimedia.org/wikipedia/commons/4/41/Profile-720.png',
+                                        bio: 'High school student')
 
-    Post.create!(title: 'Introductory Post', text: 'Hello, everyone!', comments_counter: 0,
-                 likes_counter: 2, author: @doctor)
-    Post.create!(title: 'Another intro', text: 'Greetings!', comments_counter: 0,
-                 likes_counter: 2, author: @doctor)
-    Post.create!(title: 'Getting Started', text: 'Hi there!', comments_counter: 0,
-                 likes_counter: 2, author: @doctor)
-    Post.create!(title: 'Intro to the Community', text: 'Hi, all!', comments_counter: 0,
-                 likes_counter: 2, author: @doctor)
+    Post.create!(title: 'Introductory Post', text: 'Hello, everyone!', comments_counter: 0, likes_counter: 2,
+                 author: @law_enforcement_officer)
+    Post.create!(title: 'Another Intro', text: 'Greetings!', comments_counter: 0, likes_counter: 2,
+                 author: @law_enforcement_officer)
+    Post.create!(title: 'Getting Started', text: 'Hi there!', comments_counter: 0, likes_counter: 2,
+                 author: @law_enforcement_officer)
+    Post.create!(title: 'Intro to the Community', text: 'Hi, all!', comments_counter: 0, likes_counter: 2,
+                 author: @law_enforcement_officer)
 
-    @patient_post = Post.create!(title: 'Patient Introduction', text: 'Hey, friend!',
-                                 comments_counter: 0, likes_counter: 2, author: @advent_hospital_patient)
+    @student_post = Post.create!(title: 'Student Introduction', text: 'Hey, friends!', comments_counter: 0,
+                                 likes_counter: 2, author: @high_school_student)
   end
 
   describe 'User Profile Page' do
     it 'displays specific user names' do
-      visit user_path(@doctor)
-      expect(page).to have_content('Mauricio')
-      visit user_path(@advent_hospital_patient)
-      visit(page).to have_content('Lily')
+      visit user_path(@law_enforcement_officer)
+      expect(page).to have_content('Tom')
+      visit user_path(@high_school_student)
+      expect(page).to have_content('Lily')
     end
 
-    it 'displays users\'profiles' do
-      visit user_path(@doctor)
-      expect(page).to have_css("img[src*='#{@doctor.photo}']")
-      visit user_path(@advent_hospital_patient)
-      expect(page).to have_csss("img[src*='#{@advent_hospital_patient.photo}']")
+    it 'displays users\' profiles' do
+      visit user_path(@law_enforcement_officer)
+      expect(page).to have_css("img[src*='#{@law_enforcement_officer.photo}']")
+      visit user_path(@high_school_student)
+      expect(page).to have_css("img[src*='#{@high_school_student.photo}']")
     end
 
     it 'displays the recent 3 posts for each user' do
-      visit user_path(@doctor)
-
-      expect(@doctor.three_most_recent_posts.count).to have_content(3)
-      visit user_path(@advent_hospital_patient)
-      expect(@advent_hospital_patient.posts.count).to have_content(1)
+      visit user_path(@law_enforcement_officer)
+      expect(@law_enforcement_officer.three_most_recent_posts.count).to have_content(3)
+      visit user_path(@high_school_student)
+      expect(@high_school_student.posts.count).to have_content(1)
     end
 
     it 'displays the number of posts for each user' do
-      visit user_path(@doctor)
-      expect(@doctor.posts.count).to have_content(4)
-      visit user_path(@advent_hospital_patient)
-        .expect(@advent_hospital_patient.posts.count).to have_content(1)
+      visit user_path(@law_enforcement_officer)
+      expect(@law_enforcement_officer.posts.count).to have_content(4)
+      visit user_path(@high_school_student)
+      expect(@high_school_student.posts.count).to have_content(1)
     end
 
     it 'displays the user\'s bio' do
-      visit user_path(@doctor)
-      expect(@doctor.bio).to have_content('Mastology Doctor')
-      visit user_path(@advent_hospital_patient)
-      expect(@advent_hospital_patient.bio).to
-      have_content('Advent Hospital patient')
+      visit user_path(@law_enforcement_officer)
+      expect(@law_enforcement_officer.bio).to have_content('Law enforcement officer')
+      visit user_path(@high_school_student)
+      expect(@high_school_student.bio).to have_content('High school student')
     end
 
     it 'displays a link to see all posts' do
-      visit user_path(@doctor)
+      visit user_path(@law_enforcement_officer)
       expect(page).to have_content('See all posts')
     end
 
     it 'redirects to the user show page to see all posts' do
-      visit user_path(@doctor)
+      visit user_path(@law_enforcement_officer)
       click_link('See all posts')
       sleep 15
-      expect(current_path).to eq(user_posts_path(@doctor))
+      expect(current_path).to eq(user_posts_path(@law_enforcement_officer))
     end
 
     it 'redirects to the specific post when the user clicks on it' do
-      visit user_path(@advent_hospital_patient)
-      click_link @patient_post.title
+      visit user_path(@high_school_student)
+      click_link @student_post.title
       sleep 15
-      expect(current.path).to
-      eq(user_post_path(@advent_hospital_patient, @patient_post))
+      expect(current_path).to eq(user_post_path(@high_school_student, @student_post))
     end
   end
 end
