d415252eb2ee92ade0bb4362b5189bfe4011e531
Ivonne <ivonnebenites2015@gmail.com>
2023-12-22 19:53:37 -0500

    Update file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,23 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+# class User < ApplicationRecord
+#   # Include default devise modules. Others available are:
+#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+#   devise :database_authenticatable, :registerable,
+#          :recoverable, :rememberable, :validatable,
+#          :confirmable
 
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+#   validates :name, presence: true
+#   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
-  has_one_attached :photo
+#   has_many :posts, foreign_key: 'author_id'
+#   has_many :comments
+#   has_many :likes
+#   has_one_attached :photo
 
-  def three_most_recent_posts
-    posts.order(created_at: :desc).limit(3)
-  end
+#   def three_most_recent_posts
+#     posts.order(created_at: :desc).limit(3)
+#   end
 
-  def photo_attached?
-    photo.attached?
-  end
-end
+#   def photo_attached?
+#     photo.attached?
+#   end
+# end
